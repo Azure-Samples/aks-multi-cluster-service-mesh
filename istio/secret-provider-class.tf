ebc995f63ffe744e31076b982822f5ae437c76cc
Saverio Proto <saverioproto@microsoft.com>
2022-09-15 16:09:25 +0200

    Fix regression introduced when the kv- prefix was added to the keyvault name

--- istio/secret-provider-class.tf
+++ istio/secret-provider-class.tf
@@ -1,12 +1,12 @@
 
 
 output "secret-provider-class-westeurope" {
-  value = templatefile("secret-provider-class.tftpl", { userAssignedIdentityID = module.aks-westeurope.key_vault_secrets_provider.secret_identity[0].client_id, vaultname = random_string.random.result, tenant = data.azurerm_client_config.current.tenant_id, location = "westeurope" })
+  value = templatefile("secret-provider-class.tftpl", { userAssignedIdentityID = module.aks-westeurope.key_vault_secrets_provider.secret_identity[0].client_id, vaultname = azurerm_key_vault.this.name, tenant = data.azurerm_client_config.current.tenant_id, location = "westeurope" })
 }
 output "secret-provider-class-eastus" {
-  value = templatefile("secret-provider-class.tftpl", { userAssignedIdentityID = module.aks-eastus.key_vault_secrets_provider.secret_identity[0].client_id, vaultname = random_string.random.result, tenant = data.azurerm_client_config.current.tenant_id, location = "eastus" })
+  value = templatefile("secret-provider-class.tftpl", { userAssignedIdentityID = module.aks-eastus.key_vault_secrets_provider.secret_identity[0].client_id, vaultname = azurerm_key_vault.this.name, tenant = data.azurerm_client_config.current.tenant_id, location = "eastus" })
 }
 
 output "secret-provider-class-ingress" {
-  value = templatefile("secret-provider-class-ingress.tftpl", { userAssignedIdentityID = module.aks-eastus.key_vault_secrets_provider.secret_identity[0].client_id, vaultname = random_string.random.result, tenant = data.azurerm_client_config.current.tenant_id })
+  value = templatefile("secret-provider-class-ingress.tftpl", { userAssignedIdentityID = module.aks-eastus.key_vault_secrets_provider.secret_identity[0].client_id, vaultname = azurerm_key_vault.this.name, tenant = data.azurerm_client_config.current.tenant_id })
 }

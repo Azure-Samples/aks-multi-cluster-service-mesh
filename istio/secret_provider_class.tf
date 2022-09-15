88a8f625aa07f21bb25ad27265ba7cdc6f911c57
Paolo Salvatori <paolos@microsoft.com>
2022-09-15 16:55:55 +0200

    Fixed secret_provider_class.tf

--- istio/secret_provider_class.tf
+++ istio/secret_provider_class.tf
@@ -1,11 +1,11 @@
 output "secret_provider_class_location_one" {
-  value = templatefile("secret_provider_class.tftpl", { userAssignedIdentityID = module.aks_one.key_vault_secrets_provider.secret_identity[0].client_id, vaultname = random_string.random.result, tenant = data.azurerm_client_config.current.tenant_id, location = "westeurope" })
+  value = templatefile("secret_provider_class.tftpl", { userAssignedIdentityID = module.aks_one.key_vault_secrets_provider.secret_identity[0].client_id, vaultname = azurerm_key_vault.key_vault.name, tenant = data.azurerm_client_config.current.tenant_id, location = module.aks_one.location })
 }
 
 output "secret_provider_class_location_two" {
-  value = templatefile("secret_provider_class.tftpl", { userAssignedIdentityID = module.aks_two.key_vault_secrets_provider.secret_identity[0].client_id, vaultname = random_string.random.result, tenant = data.azurerm_client_config.current.tenant_id, location = "eastus" })
+  value = templatefile("secret_provider_class.tftpl", { userAssignedIdentityID = module.aks_two.key_vault_secrets_provider.secret_identity[0].client_id, vaultname = azurerm_key_vault.key_vault.name, tenant = data.azurerm_client_config.current.tenant_id, location = module.aks_two.location })
 }
 
 output "secret_provider_class_ingress" {
-  value = templatefile("secret_provider_class_ingress.tftpl", { userAssignedIdentityID = module.aks_two.key_vault_secrets_provider.secret_identity[0].client_id, vaultname = random_string.random.result, tenant = data.azurerm_client_config.current.tenant_id })
+  value = templatefile("secret_provider_class_ingress.tftpl", { userAssignedIdentityID = module.aks_two.key_vault_secrets_provider.secret_identity[0].client_id, vaultname = azurerm_key_vault.key_vault.name, tenant = data.azurerm_client_config.current.tenant_id })
 }

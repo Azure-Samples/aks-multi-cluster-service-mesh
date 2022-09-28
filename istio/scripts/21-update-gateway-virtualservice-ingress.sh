1389749feb17529e0af9a3e74dc1652653e589fb
Saverio Proto <saverioproto@microsoft.com>
2022-09-28 11:02:13 +0200

    bugfix: Set --context when calling kubectl

--- istio/scripts/21-update-gateway-virtualservice-ingress.sh
+++ istio/scripts/21-update-gateway-virtualservice-ingress.sh
@@ -2,9 +2,12 @@
 
 # Variables
 yamlDir="../yaml"
+prefix="ifwfhi"
+aksClusterOneLocation="westeurope"
+aksClusterOneName="$prefix-$aksClusterOneLocation-aks-one"
 
 # Change the working directory to the yaml folder
 cd $yamlDir
 
 # Update
-kubectl apply -f gateway-tls.yaml -f virtualservice-tls.yaml -f ingress-tls-e2e.yaml
+kubectl --context=$aksClusterOneName apply -f gateway-tls.yaml -f virtualservice-tls.yaml -f ingress-tls-e2e.yaml
